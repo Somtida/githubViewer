5b272ea47b4b925809a8b6abfdc61c7774d0e925
S.SKNS <somtida.th@gmail.com>
2016-12-31 14:38:40 -0800

    list

--- script.js
+++ script.js
@@ -1,32 +1,38 @@
 (function() {
-  
+
 
   var app = angular.module('myApp',[]);
-  
+
   var mainCtrl = function($scope, $http) {
    // $scope.message = "Hello Y'all";
-    
+
     $scope.inputName = "somtida";
-    
-    var complete = (res) => {
+
+    var completedUser = (res) => {
       $scope.user = res.data;
+      $http.get($scope.user.repos_url)
+        .then(completedRepo, error)
     }
-    
+
+    var completedRepo = (res) => {
+      $scope.repos = res.data;
+    }
+
     var error = () => {
       $scope.error = "Counld not fetch the user";
     }
-   
+
     $scope.search = () => {
       $http.get("https://api.github.com/users/"+$scope.inputName)
-        .then(complete, error);
+        .then(completedUser, error);
     }
-    
-      
-    $scope.message = "Hello!"
-   
+
+
+
+
   }
-  
-  app.controller("mainCtrl", ["$scope", "$http", mainCtrl]) 
-  
-  
+
+  app.controller("mainCtrl", ["$scope", "$http", mainCtrl])
+
+
 }());
