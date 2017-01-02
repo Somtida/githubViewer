21faccfe89f6f1d95d4d45da970366cab13b6c85
S.SKNS <somtida.th@gmail.com>
2017-01-01 21:52:20 -0800

    interval

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 
   var app = angular.module('myApp',[]);
 
-  var mainCtrl = function($scope, $http) {
+  var mainCtrl = function($scope, $http, $interval) {
    // $scope.message = "Hello Y'all";
 
     $scope.inputName = "somtida";
@@ -22,17 +22,31 @@
       $scope.error = "Counld not fetch the user";
     }
 
-    $scope.search = () => {
-      $http.get("https://api.github.com/users/"+$scope.inputName)
+    $scope.search = (inputName) => {
+      $http.get("https://api.github.com/users/"+inputName)
         .then(completedUser, error);
     }
+
+    var decrementCountdown = () => {
+      $scope.countdown -= 1;
+      if($scope.countdown < 1) {
+        $scope.search($scope.inputName);
+      }
+    }
+
+    var startCountdown = () => {
+      $interval(decrementCountdown, 1000, $scope.countdown);
+    }
+
+    $scope.countdown = 5;
+    startCountdown();
 
 
 
 
   }
 
-  app.controller("mainCtrl", ["$scope", "$http", mainCtrl])
+  app.controller("mainCtrl", ["$scope", "$http", "$interval", mainCtrl])
 
 
 }());
